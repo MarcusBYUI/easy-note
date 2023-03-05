88db2fcd1e84b1ccf8351b6786f4a5eb0e331326
MarcusBYUI <ojo18002@byui.edu>
2023-03-05 06:00:32 +0100

    .

--- src/components/dashboard/settings/settings.js
+++ src/components/dashboard/settings/settings.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import NotesHeader from "../notes/components/notesHeader/notesHeader";
 
 import styles from "./settings.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUser } from "../../home/authhelper";
 
 const Settings = () => {
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const authState = useSelector((state) => state.auth.loggedIn);
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    updateUser(dispatch, form, authState);
+  };
+
+  const [first_name, setFirst_name] = useState(user.first_name);
+  const [last_name, setLast_name] = useState(user.last_name);
+
   return (
     <main className={styles.settings}>
       <NotesHeader type={true} />
-      <form onSubmit="">
+      <form onSubmit={handleUpdate}>
         <label>
-          Name
-          <input type="text" name="name" required />
+          First Name
+          <input
+            type="text"
+            name="first_name"
+            value={first_name}
+            onChange={(e) => setFirst_name(e.target.value)}
+          />
         </label>
         <label>
+          Last Name
+          <input
+            type="text"
+            name="last_name"
+            value={last_name}
+            onChange={(e) => setLast_name(e.target.value)}
+          />
+        </label>
+
+        <label>
           Password
-          <input type="password" name="password" required />
+          <input type="password" name="password" />
         </label>
         <input type="submit" value="Update" />
       </form>
--- src/components/home/authhelper.js
+++ src/components/home/authhelper.js
@@ -9,6 +9,7 @@ export const signup = async (dispatch, form, setScreen) => {
   try {
     await apiRequest("api/auth/register", form, "POST");
     setScreen("signin");
+    dispatch(notificationActions.setNotify(false));
   } catch (error) {
     if (error.info) {
       dispatch(notificationActions.setMessage(error.info));
@@ -30,6 +31,20 @@ export const login = async (dispatch, form, navigate) => {
     setLocalStorage("expiry", new Date().getTime() + 36000000);
     dispatch(notificationActions.setNotify(false));
     navigate("/user");
+  } catch (error) {
+    if (error.info) {
+      dispatch(notificationActions.setMessage(error.info));
+    } else {
+      dispatch(notificationActions.setMessage("Something went wrong"));
+    }
+  }
+};
+
+export const updateUser = async (dispatch, body, auth) => {
+  dispatch(notificationActions.setNotify(true));
+  try {
+    await apiRequest("api/auth/update", body, "patch", auth);
+    dispatch(notificationActions.setNotify(false));
   } catch (error) {
     if (error.info) {
       dispatch(notificationActions.setMessage(error.info));
