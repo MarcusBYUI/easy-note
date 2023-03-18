9c4a906312241daba44c2c7e134b9a62f22b51aa
MarcusBYUI <ojo18002@byui.edu>
2023-03-18 20:59:03 +0100

    .

--- src/helpers/connections.js
+++ src/helpers/connections.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//const BASEURL = "https://backend.onrender.com/";
-const BASEURL = "http://localhost:8000/";
+const BASEURL = "http://100.25.196.27:8000/";
+//const BASEURL = "http://localhost:8000/";
 export async function apiRequest(path, body = {}, method = "get", auth) {
   return new Promise(async (resolve, reject) => {
     let header = {};
